6787f8080ddfb4f008e4c70726df661eafc13032
doinki <doinki94@gmail.com>
2022-05-07 14:23:52 +0900

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({ dir: '.' });
 
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
 const customJestConfig = {
+  collectCoverageFrom: ['src/**/*.@(j|t)s?(x)'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
 };
